c391c4c90923c498d07c890f021b7ee8e85f631c
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-02-28 13:55:36 -0300

    Inicia projeto

--- api/wanderlust/src/test/java/com/wanderlust/wanderlust/WanderlustApplicationTests.java
+++ api/wanderlust/src/test/java/com/wanderlust/wanderlust/WanderlustApplicationTests.java
@@ -1,10 +1,10 @@
-package com.wanderlust;
+package com.wanderlust.wanderlust;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PadariaApplicationTests {
+class WanderlustApplicationTests {
 
 	@Test
 	void contextLoads() {
