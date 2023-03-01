548ac7c3928a6c6c11edc1fe84d901f8e4e591d9
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-03-01 20:57:27 -0300

    Implementa tag das rotas para utilizar no swagger

--- api/wanderlust/src/main/java/com/wanderlust/wanderlust/controller/airport/AirportController.java
+++ api/wanderlust/src/main/java/com/wanderlust/wanderlust/controller/airport/AirportController.java
@@ -4,6 +4,8 @@ import com.wanderlust.wanderlust.external.aeroDataBox.AeroDataBoxService;
 import com.wanderlust.wanderlust.external.aeroDataBox.model.aiportInfo.Airport;
 import com.wanderlust.wanderlust.external.aeroDataBox.model.aiportRoutesDailyFlight.AiportRoutesDailyFlight;
 import com.wanderlust.wanderlust.external.aeroDataBox.model.airportSearchResult.AirportSearchResult;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -14,22 +16,26 @@ import javax.validation.constraints.NotNull;
 @Validated
 @RestController
 @RequestMapping("api/aeroporto")
+@Tag(name = "Consulta de aeroportos")
 public class AirportController {
 
     @Autowired
     AeroDataBoxService aeroDataBoxService;
 
     @GetMapping("info/{airportDataCode}")
+    @Operation(summary = "Consulta informação do aeroporto pela IATA")
     public Airport findAirportByIataCode(@PathVariable @NotNull @NotBlank String airportDataCode) {
         return aeroDataBoxService.findAirportByIataCode(airportDataCode);
     }
 
     @GetMapping("search/{airportName}")
+    @Operation(summary = "Consulta informação do aeroporto pelo nome")
     public AirportSearchResult findAirportBySearchName(@PathVariable @NotNull @NotBlank String airportName) {
         return aeroDataBoxService.findAirportBySearchName(airportName);
     }
 
     @GetMapping("routes/{airportIcaoCode}")
+    @Operation(summary = "Consulta rotas de voos do aeroporto pelo ICAO")
     public AiportRoutesDailyFlight findAirportRoutes(@PathVariable @NotNull @NotBlank String airportIcaoCode) {
         return aeroDataBoxService.findAirportRoutes(airportIcaoCode);
     }
