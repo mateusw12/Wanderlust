e643ae068f379f5bc9964a8fbd9df23c7189b948
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-03-02 20:43:48 -0300

    Implementa consulta de aeroportos e voos

--- api/wanderlust/src/main/java/com/wanderlust/wanderlust/controller/airport/AirportController.java
+++ api/wanderlust/src/main/java/com/wanderlust/wanderlust/controller/airport/AirportController.java
@@ -1,9 +1,15 @@
 package com.wanderlust.wanderlust.controller.airport;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.wanderlust.wanderlust.external.aeroDataBox.AeroDataBoxService;
 import com.wanderlust.wanderlust.external.aeroDataBox.model.aiportInfo.Airport;
 import com.wanderlust.wanderlust.external.aeroDataBox.model.aiportRoutesDailyFlight.AiportRoutesDailyFlight;
 import com.wanderlust.wanderlust.external.aeroDataBox.model.airportSearchResult.AirportSearchResult;
+import com.wanderlust.wanderlust.external.airportTimeTable.AirportTimeTableService;
+import com.wanderlust.wanderlust.external.airportTimeTable.model.airportCity.AirportCity;
+import com.wanderlust.wanderlust.external.airportTimeTable.model.airportCountry.AirportCountry;
+import com.wanderlust.wanderlust.external.airportTimeTable.model.airports.AirportTimeTable;
+import com.wanderlust.wanderlust.external.airportTimeTable.model.country.AirportResponse;
 import com.wanderlust.wanderlust.external.flightInfo.FlightInfoService;
 import com.wanderlust.wanderlust.external.flightInfo.model.status.FlighData;
 import com.wanderlust.wanderlust.external.flightInfo.model.status.filter.FlighStatusFilter;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Validated
 @RestController
@@ -27,6 +34,9 @@ public class AirportController {
 
     @Autowired
     FlightInfoService flightInfoService;
+
+    @Autowired
+    AirportTimeTableService airportTimeTableService;
 
     @GetMapping("/info/{airportDataCode}")
     @Operation(summary = "Consulta informação do aeroporto pela IATA")
@@ -46,14 +56,38 @@ public class AirportController {
         return aeroDataBoxService.findAirportRoutes(airportIcaoCode);
     }
 
-    @PostMapping("/fligh-status")
+    @GetMapping
+    @Operation(summary = "Consulta todos os aeroportos")
+    public List<AirportTimeTable> findAirports() throws JsonProcessingException {
+        return airportTimeTableService.findAirports();
+    }
+
+    @GetMapping("/country/{countryIataCode}")
+    @Operation(summary = "Consulta todos os aeroportos")
+    public AirportResponse findAirports(@PathVariable @NotNull @NotBlank String countryIataCode) throws JsonProcessingException {
+        return airportTimeTableService.findAirportsByCountry(countryIataCode);
+    }
+
+    @GetMapping("/cities")
+    @Operation(summary = "Consulta cidades principais com aeroportos")
+    public List<AirportCity> findAirportCity() throws JsonProcessingException {
+        return airportTimeTableService.findAirportCity();
+    }
+
+    @GetMapping("/countries")
+    @Operation(summary = "Consulta codigos iata dos países")
+    public List<AirportCountry> findAirportsByCountry() throws JsonProcessingException {
+        return airportTimeTableService.findAIataCodeByCountry();
+    }
+
+    @PostMapping("/flight-status")
     @Operation(summary = "Consulta status dos voos com filtros")
     public FlighData findFlighsStatus(@RequestBody FlighStatusFilter filter) {
         return flightInfoService.findFlightsStatus(filter);
     }
 
     @PostMapping("/fligh-schedules")
-    @Operation(summary = "Consulta agendameno de voos com filtros")
+    @Operation(summary = "Consulta agendamento de voos com filtros")
     public FlighData findFlightsSchedules(@RequestBody FlighStatusFilter filter) {
         return flightInfoService.findFlightsSchedules(filter);
     }
