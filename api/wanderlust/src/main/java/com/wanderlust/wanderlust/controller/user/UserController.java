50079224d878d5e4675a323da2e116941c072034
Mateus Walz <mateus.walz@gmail.com>
2023-03-01 21:01:11 -0300

    Implementa tag das rotas para utilizar no swagger (#10)
    * Implementa tag das rotas para utilizar no swagger
    
    * Implementa tag das rotas do email para utilizar no swagger
    
    * Implementa tag das rotas do cadastro de usuario para utilizar no swagger
    
    * Ajusta rotas no controle de aeroportos

--- api/wanderlust/src/main/java/com/wanderlust/wanderlust/controller/user/UserController.java
+++ api/wanderlust/src/main/java/com/wanderlust/wanderlust/controller/user/UserController.java
@@ -2,6 +2,8 @@ package com.wanderlust.wanderlust.controller.user;
 
 import com.wanderlust.wanderlust.dto.user.UserDTO;
 import com.wanderlust.wanderlust.service.user.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 @Validated
 @RestController
 @RequestMapping("api/cadastro/usuario")
+@Tag(name = "Cadastro de usuário")
 public class UserController {
 
     private final UserService userService;
@@ -23,28 +26,33 @@ public class UserController {
     }
 
     @GetMapping
+    @Operation(summary = "Consulta todos os usuários")
     public @ResponseBody List<UserDTO> findAll() {
         return userService.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Consulta usuário por código")
     public UserDTO findById(@PathVariable @NotNull @Positive Long id) {
         return userService.findById(id);
     }
 
     @PostMapping
+    @Operation(summary = "Cadastra um usuário")
     @ResponseStatus(code = HttpStatus.CREATED)
     public UserDTO create(@RequestBody @Valid @NotNull UserDTO userDTO) {
         return userService.create(userDTO);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualiza um usuário")
     public UserDTO update(@PathVariable @NotNull @Positive Long id,
                             @RequestBody @Valid @NotNull UserDTO userDTO) {
         return userService.update(id, userDTO);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Exclui um usuário")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void delete(@PathVariable @NotNull @Positive Long id) {
         userService.delete(id);
