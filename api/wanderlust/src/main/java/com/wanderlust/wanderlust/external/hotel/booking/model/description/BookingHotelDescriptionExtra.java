e9a4af7f8fbbe4f1ede7c4f1446e889fc56710c5
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-03-06 14:28:00 -0300

    Implementa consulta de descrição do hotel na api do booking

---
+++ api/wanderlust/src/main/java/com/wanderlust/wanderlust/external/hotel/booking/model/description/BookingHotelDescriptionExtra.java
@@ -0,0 +1,10 @@
+package com.wanderlust.wanderlust.external.hotel.booking.model.description;
+
+import lombok.Data;
+
+@Data
+public class BookingHotelDescriptionExtra {
+
+    private String imp_info;
+
+}
