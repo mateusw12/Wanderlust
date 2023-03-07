d8459c9d0a09c2dbad91e1fe0c694e4151f1e9f8
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-03-07 11:25:08 -0300

    Implementa integração com a api do Brazilian Airline para consulta de voos por companhia aerea

---
+++ api/wanderlust/src/main/java/com/wanderlust/wanderlust/external/airportAndFlight/brazilianAirlineData/BrazilianArlineDataService.java
@@ -0,0 +1,49 @@
+package com.wanderlust.wanderlust.external.airportAndFlight.brazilianAirlineData;
+
+import com.wanderlust.wanderlust.external.airportAndFlight.brazilianAirlineData.model.BrazilianAirlineDataFilter;
+import com.wanderlust.wanderlust.external.airportAndFlight.brazilianAirlineData.model.BrazilianArilineData;
+import com.wanderlust.wanderlust.external.carRental.skyscanner.model.SkyscannerSearchLocation;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+@Service
+public class BrazilianArlineDataService {
+
+    private final String apiKey = "d5075a53bemshe3df1656b7affc0p136397jsnf9c8d0f8ddbc";
+
+    public List<BrazilianArilineData> findAirlineData(BrazilianAirlineDataFilter filter) {
+        String apiUrl = "https://brazilian-airlines-real-flights-data.p.rapidapi.com/flights?";
+
+        if(filter.getCompany() != null){
+            apiUrl = apiUrl + "company="+filter.getCompany();
+        }
+
+        if(filter.getDate() != null){
+            apiUrl = apiUrl + "&date="+filter.getDate();
+        }
+
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.set("X-RapidAPI-Key", apiKey);
+        headers.set("X-RapidAPI-Host", "brazilian-airlines-real-flights-data.p.rapidapi.com");
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<List<BrazilianArilineData>> response = restTemplate.exchange(
+                apiUrl,
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<List<BrazilianArilineData>>() {}
+        );
+        List<BrazilianArilineData> brazilianArilineDataList = response.getBody();
+        return brazilianArilineDataList;
+    }
+
+}
---
+++ api/wanderlust/src/main/java/com/wanderlust/wanderlust/external/airportAndFlight/brazilianAirlineData/model/BrazilianAirlineDataFlightDays.java
@@ -0,0 +1,16 @@
+package com.wanderlust.wanderlust.external.airportAndFlight.brazilianAirlineData.model;
+
+import lombok.Data;
+
+@Data
+public class BrazilianAirlineDataFlightDays {
+
+    private Boolean friday;
+    private Boolean monday;
+    private Boolean saturday;
+    private Boolean thursday;
+    private Boolean sunday;
+    private Boolean tuesday;
+    private Boolean wednesday;
+
+}
