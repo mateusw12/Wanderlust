483b151e15966fe96db7f1229dc4540c7f41f8cd
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-03-08 15:33:27 -0300

    Implementa validação de usuário logado

--- api/wanderlust/src/main/java/com/wanderlust/wanderlust/validator/user/UserCustomValidation.java
+++ api/wanderlust/src/main/java/com/wanderlust/wanderlust/validator/user/UserCustomValidation.java
@@ -2,6 +2,7 @@ package com.wanderlust.wanderlust.validator.user;
 
 import com.wanderlust.wanderlust.model.user.UserModel;
 import com.wanderlust.wanderlust.repository.user.UserRepository;
+import com.wanderlust.wanderlust.security.utils.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,9 +12,18 @@ public class UserCustomValidation {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private JWTUtil jwtUtil;
+
     public boolean validateActiveUser(String userName) {
         UserModel userModel = userRepository.findByUserName(userName);
         return userModel.getIsActive();
+    }
+
+    public boolean validadeLoggedUser(String userName) {
+        UserModel userModel = userRepository.findByUserName(userName);
+        String loggedUser = jwtUtil.findMe();
+        return userModel.getUserName() == loggedUser;
     }
 
 }
