ff2e278e0dd2a54df4f7f9ec26a8a0bf74bb87c9
Mateus Walz <mateus.walz@gmail.com>
2023-03-09 18:32:38 -0300

    Implementa validações extras nos dados do cartão de crédito (#46)
    * Implementa validação nas informações adicionais no cartao de crédito
    
    * Implementa validação de tamanho máximo de caracteres do cartao de crédito

--- api/wanderlust/src/main/java/com/wanderlust/wanderlust/external/payment/asas/bankChargers/model/creditCard/BankChargerCreditCardInfo.java
+++ api/wanderlust/src/main/java/com/wanderlust/wanderlust/external/payment/asas/bankChargers/model/creditCard/BankChargerCreditCardInfo.java
@@ -2,16 +2,30 @@ package com.wanderlust.wanderlust.external.payment.asas.bankChargers.model.credi
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Data
 public class BankChargerCreditCardInfo {
 
+    @NotNull
+    @NotBlank
     private String name;
+
     private String email;
+
+    @NotNull
+    @NotBlank
     private String cpfCnpj;
+
     private String postalCode;
+
     private String addressNumber;
+
     private String addressComplement;
+
     private String phone;
+
     private String mobilePhone;
 
 }
