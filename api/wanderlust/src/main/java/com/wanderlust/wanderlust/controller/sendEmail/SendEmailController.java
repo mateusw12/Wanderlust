dad6701c66274ff5035e1d7aa014ebd033bd719a
mateusw12 <38771418+mateusw12@users.noreply.github.com>
2023-03-10 11:23:01 -0300

    Implementa validações nas propriedades do envio de email

--- api/wanderlust/src/main/java/com/wanderlust/wanderlust/controller/sendEmail/SendEmailController.java
+++ api/wanderlust/src/main/java/com/wanderlust/wanderlust/controller/sendEmail/SendEmailController.java
@@ -6,13 +6,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.mail.MessagingException;
+import javax.validation.Valid;
 
+@Validated
 @RestController
 @RequestMapping("api/envio-email")
 @Tag(name = "Controle de envio de E-mail")
@@ -23,7 +26,7 @@ public class SendEmailController {
 
     @PostMapping
     @Operation(summary = "Envia email")
-    public ResponseEntity<Email> sendEmail(@RequestBody Email email) throws MessagingException {
+    public ResponseEntity<Email> sendEmail(@RequestBody @Valid Email email) throws MessagingException {
         emailService.sendEmail(email);
         return ResponseEntity.ok().build();
     }
--- api/wanderlust/src/main/java/com/wanderlust/wanderlust/model/sendEmail/Email.java
+++ api/wanderlust/src/main/java/com/wanderlust/wanderlust/model/sendEmail/Email.java
@@ -1,14 +1,27 @@
 package com.wanderlust.wanderlust.model.sendEmail;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Data
 public class Email {
 
+    @NotNull
+    @NotBlank
+    @Length(min = 1, max = 200)
     public String subject;
 
+    @NotNull
+    @NotBlank
+    @Length(min = 1, max = 2000)
     public String body;
 
+    @NotNull
+    @NotBlank
+    @javax.validation.constraints.Email
     public String recipient;
 
 }
